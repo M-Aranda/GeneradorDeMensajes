6b9b5d6884241acdd68137abf3f8596ae28e4ed3
Marcelo <cheloz_20@hotmail.com>
2022-07-14 16:33:14 -0300

    Ahora se consideran sabados y domingos de la proxima semana a la hora de hacer recordatorios. También se itera sobre todas las hojas del Excel, en lugar de, solo la primera. Aún queda ajustar los words que se generan (la presentacion)

--- Form1.cs
+++ Form1.cs
@@ -27,6 +27,11 @@ namespace GeneradorDeMensajes
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
             //programa debe tomar Excel de una hoja y generar varios words a partir de la información presente en dicho Excel
+
+            //actualizacion 14/07/2022
+            //El programa debe hacer 2 cosas: primero generar un word de todas las demandas que se derivaron el día actual (si es que
+            //no estan generados ya). Además, debe generar un reccordatorio con todas las demandas la cuya audiencia real sea la semana
+            //siguiente (independiente de si el día en cuestión es no hábil). 
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -59,7 +64,7 @@ namespace GeneradorDeMensajes
             string downloads = @"C:\Users\" + username + @"\Downloads";
 
 
-            int contadorDeWordsCreados = 0;
+            int contadorDeWordsCreadosConFechaDeDerivacionActual = 0;
 
 
 
@@ -107,54 +112,63 @@ namespace GeneradorDeMensajes
                     item.Pjud = soloLaFechaPjudComoString;
 
                 }
-                
+
+
 
                 //si la fecha actual es igual a la fecha de derivacion, entonces se crea el word, para hoy
-                if ((item.RolOficio!= "Rol Oficio" && item.RolOficio != null) && (soloLaFechaActualComoString==soloLaFechaDeDerivacionComoString))
+                if ((item.RolOficio != "Rol Oficio" && item.RolOficio != null) && (soloLaFechaActualComoString == soloLaFechaDeDerivacionComoString))
                 {
                     String archivo = downloads + @"\Rol oficio " + item.RolOficio + ".docx";
 
-                    var doc = DocX.Create(archivo);
+                    if (File.Exists(archivo))//archivo existe, no se crea nada
+                    {
 
-                    //titulo que va a llevar el word
-                    string title = "Estimado/a " + item.Asignado + ",";
+                    }
+                    else//archivo no existe, asi que se crea
+                    {
 
-                    //texto a escribir en el word
-                    string textParagraph = "usted a sido asignado/a para el " + item.Tribunal + " el día " + item.FechaDeAudienciaReal + " por el rol de oficio " + item.RolOficio + " de la Isapre"
-                        + item.Isapre + ", entre las partes " + item.Partes + ". En materia de " + item.Materia + ". " + Environment.NewLine
-                        + "La fecha de derivacion fue el " + item.FechaDeDerivacion + ". La fecha en que los antecedentes fueron enviados fue el " + item.FechaDeAntecedentes + " y se encuentran en estado" + item.AntecedentesEnviados
-                        + ". El pjud es " + item.Pjud + " y el folio está " + item.Folio;
+                        var doc = DocX.Create(archivo);
 
+                        //titulo que va a llevar el word
+                        string title = "Estimado/a " + item.Asignado + ",";
 
-
-                    Formatting titleFormat = new Formatting();
-                    //Specify font family  
-                    titleFormat.FontFamily = new Xceed.Document.NET.Font("Times New Roman");
-                    //Specify font size  
-                    titleFormat.Size = 12;
-                    titleFormat.Position = 40;
-                    titleFormat.FontColor = System.Drawing.Color.Black;
-                    //titleFormat.UnderlineColor = System.Drawing.Color.Gray;
-                    //titleFormat.Italic = true;
-
-                    //Formatting Text Paragraph  
-                    Formatting textParagraphFormat = new Formatting();
-                    //font family  
-                    textParagraphFormat.FontFamily = new Xceed.Document.NET.Font("Times New Roman");
-                    //font size  
-                    textParagraphFormat.Size = 12;
-                    //Spaces between characters  
-                    textParagraphFormat.Spacing = 2;
+                        //texto a escribir en el word
+                        string textParagraph = "usted a sido asignado/a para el " + item.Tribunal + " el día " + item.FechaDeAudienciaReal + " por el rol de oficio " + item.RolOficio + " de la Isapre"
+                            + item.Isapre + ", entre las partes " + item.Partes + ". En materia de " + item.Materia + ". " + Environment.NewLine
+                            + "La fecha de derivacion fue el " + item.FechaDeDerivacion + ". La fecha en que los antecedentes fueron enviados fue el " + item.FechaDeAntecedentes + " y se encuentran en estado" + item.AntecedentesEnviados
+                            + ". El pjud es " + item.Pjud + " y el folio está " + item.Folio;
 
 
-                    doc.InsertParagraph(title, false, titleFormat);
-                    doc.InsertParagraph(textParagraph, false, titleFormat);//textParagraphFormat
+
+                        Formatting titleFormat = new Formatting();
+                        //Specify font family  
+                        titleFormat.FontFamily = new Xceed.Document.NET.Font("Times New Roman");
+                        //Specify font size  
+                        titleFormat.Size = 12;
+                        // titleFormat.Position = 40;
+                        titleFormat.FontColor = System.Drawing.Color.Black;
+                        //titleFormat.UnderlineColor = System.Drawing.Color.Gray;
+                        //titleFormat.Italic = true;
+
+                        //Formatting Text Paragraph  
+                        Formatting textParagraphFormat = new Formatting();
+                        //font family  
+                        textParagraphFormat.FontFamily = new Xceed.Document.NET.Font("Times New Roman");
+                        //font size  
+                        textParagraphFormat.Size = 12;
+                        //Spaces between characters  
+                        // textParagraphFormat.Spacing = 2;
 
 
-                    doc.Save();
-                    contadorDeWordsCreados++;
+                        doc.InsertParagraph(title, false, titleFormat);
+                        doc.InsertParagraph(textParagraph, false, titleFormat);//textParagraphFormat
 
 
+                        doc.Save();
+                        contadorDeWordsCreadosConFechaDeDerivacionActual++;
+
+
+                    }
                 }
 
 
@@ -162,8 +176,10 @@ namespace GeneradorDeMensajes
 
 
 
+            //si el día en el que corre el proceso, es un viernes, entonces deben crearse words de recordatorio de 
+            //las semanas que viene (para enviarse ese mismo viernes, o sea, la fecha actual)
             var x = DateTime.Now;
-            if (x.DayOfWeek != DayOfWeek.Friday)
+            if (x.DayOfWeek == DayOfWeek.Friday)
             {
                 Console.WriteLine("Es viernes");
                 //deben crearse documentos extra
@@ -191,8 +207,10 @@ namespace GeneradorDeMensajes
                         var proximoMiercoles = j.AddDays(5).ToString();
                         var proximoJueves = j.AddDays(6).ToString();
                         var proximoViernes = j.AddDays(7).ToString();
+                        var proximoSabado = j.AddDays(8).ToString();
+                        var proximoDomingo = j.AddDays(9).ToString();
 
-                     
+
                         string[] words = proximoLunes.Split(' ');
                         String proximoLunesSinTiempo = words[0];
 
@@ -208,18 +226,27 @@ namespace GeneradorDeMensajes
                          words = proximoViernes.Split(' ');
                         String proximoViernesSinTiempo = words[0];
 
+                        words = proximoSabado.Split(' ');
+                        String proximoSabadoSinTiempo = words[0];
+
+                        words = proximoDomingo.Split(' ');
+                        String proximoDomingoSinTiempo = words[0];
+
                         List<String> proximasFechas=new List<String>();
                         proximasFechas.Add(proximoLunesSinTiempo);
                         proximasFechas.Add(proximoMartesSinTiempo);
                         proximasFechas.Add(proximoMiercolesSinTiempo);
                         proximasFechas.Add(proximoJuevesSinTiempo);
                         proximasFechas.Add(proximoViernesSinTiempo);
+                        proximasFechas.Add(proximoSabadoSinTiempo);
+                        proximasFechas.Add(proximoDomingoSinTiempo);
 
                         String[] listadoDeProximasFechas = proximasFechas.ToArray();
 
                         if (item.ToString()==item2.Asignado && listadoDeProximasFechas.Contains(item2.FechaDeAudienciaReal))//si el registro
                         {
-                            proximasDemandas += "el día "+item2.FechaDeAudienciaReal+" "+Environment.NewLine;
+                            proximasDemandas += "Demanda " + item2.RolOficio + " entre " + item2.Partes + "el día " + item2.FechaDeAudienciaReal + " en " + item2.Tribunal + ". " + Environment.NewLine;
+                     
                             contadorDeDemandasDeLaSemanaQueViene++;
                         }
                     }
@@ -230,46 +257,55 @@ namespace GeneradorDeMensajes
 
                     String archivo = downloads + @"\Recordatorio para " + item.ToString() + ".docx";
 
-                    var doc = DocX.Create(archivo);
 
-                    //titulo que va a llevar el word
-                    string title = "Estimado/a " + item.ToString() + ",";
-
-                    //texto a escribir en el word
-                    string textParagraph = "se le recuerda que tiene demandas asignadas para la semana que viene. Especificamente los días:"+Environment.NewLine
-                        +proximasDemandas;
-
-
-
-                    Formatting titleFormat = new Formatting();
-                    //Specify font family  
-                    titleFormat.FontFamily = new Xceed.Document.NET.Font("Times New Roman");
-                    //Specify font size  
-                    titleFormat.Size = 12;
-                    titleFormat.Position = 40;
-                    titleFormat.FontColor = System.Drawing.Color.Black;
-                    //titleFormat.UnderlineColor = System.Drawing.Color.Gray;
-                    //titleFormat.Italic = true;
-
-                    //Formatting Text Paragraph  
-                    Formatting textParagraphFormat = new Formatting();
-                    //font family  
-                    textParagraphFormat.FontFamily = new Xceed.Document.NET.Font("Times New Roman");
-                    //font size  
-                    textParagraphFormat.Size = 12;
-                    //Spaces between characters  
-                    textParagraphFormat.Spacing = 2;
-
-
-                    doc.InsertParagraph(title, false, titleFormat);
-                    doc.InsertParagraph(textParagraph, false, titleFormat);//textParagraphFormat
-
-                    if (contadorDeDemandasDeLaSemanaQueViene>0)
+                    if (File.Exists(archivo))//archivo existe, no se crea nada
                     {
-                        doc.Save();
-                    }
-                    
 
+                    }
+                    else//archivo no existe, asi que se crea
+                    {
+
+                        var doc = DocX.Create(archivo);
+
+                        //titulo que va a llevar el word
+                        string title = "Estimado/a " + item.ToString() + ",";
+
+                        //texto a escribir en el word
+                        string textParagraph = title + " se le recuerda que tiene demandas asignadas para la semana que viene. Especificamente: " + Environment.NewLine
+                            + proximasDemandas;
+
+
+
+                        Formatting titleFormat = new Formatting();
+                        //Specify font family  
+                        titleFormat.FontFamily = new Xceed.Document.NET.Font("Times New Roman");
+                        //Specify font size  
+                        titleFormat.Size = 12;
+                        titleFormat.Position = 40;
+                        titleFormat.FontColor = System.Drawing.Color.Black;
+                        //titleFormat.UnderlineColor = System.Drawing.Color.Gray;
+                        //titleFormat.Italic = true;
+
+                        //Formatting Text Paragraph  
+                        Formatting textParagraphFormat = new Formatting();
+                        //font family  
+                        textParagraphFormat.FontFamily = new Xceed.Document.NET.Font("Times New Roman");
+                        //font size  
+                        textParagraphFormat.Size = 12;
+                        //Spaces between characters  
+                        //   textParagraphFormat.Spacing = 2;
+
+
+                        // doc.InsertParagraph(title, false, titleFormat);
+                        doc.InsertParagraph(textParagraph, false, titleFormat);//textParagraphFormat
+
+                        if (contadorDeDemandasDeLaSemanaQueViene > 0)
+                        {
+                            doc.Save();
+                        }
+
+
+                    }
 
                 }
 
@@ -283,13 +319,13 @@ namespace GeneradorDeMensajes
 
 
 
-            if (contadorDeWordsCreados > 1)
+            if (contadorDeWordsCreadosConFechaDeDerivacionActual > 1)
             {
-                MessageBox.Show("Se crearon "+contadorDeWordsCreados.ToString()+" documentos en la carpeta de descargas para enviar hoy");
-            }else if (contadorDeWordsCreados==1)
+                MessageBox.Show("Se crearon "+contadorDeWordsCreadosConFechaDeDerivacionActual.ToString()+" documentos en la carpeta de descargas para enviar hoy");
+            }else if (contadorDeWordsCreadosConFechaDeDerivacionActual==1)
             {
                 MessageBox.Show("Se creó un documento en la carpeta de descargas para enviar hoy");
-            }else if (contadorDeWordsCreados==0)
+            }else if (contadorDeWordsCreadosConFechaDeDerivacionActual==0)
             {
                 MessageBox.Show("No se creó ningún documento para enviar hoy");
             }
@@ -307,46 +343,44 @@ namespace GeneradorDeMensajes
             FileInfo existingFile = new FileInfo(sFileName);
             using (ExcelPackage package = new ExcelPackage(existingFile))
             {
-                //get the first worksheet in the workbook
-                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-                int colCount = worksheet.Dimension.End.Column;  //get Column Count
-                int rowCount = worksheet.Dimension.End.Row;     //get row count
+
+                int cantidadDeHojas = package.Workbook.Worksheets.Count;
 
 
-                for (int row = 1; row <= rowCount; row++)
+                for (int i = 0; i < cantidadDeHojas; i++)
                 {
 
-                    Derivacion d =  new Derivacion();   
+                    //get the first worksheet in the workbook
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[i];
+                    int colCount = worksheet.Dimension.End.Column;  //get Column Count
+                    int rowCount = worksheet.Dimension.End.Row;     //get row count
 
-                    d.RolOficio = worksheet.Cells[row, 1].Value?.ToString().Trim();
-                    d.Partes = worksheet.Cells[row, 2].Value?.ToString().Trim();
-                    d.Isapre = worksheet.Cells[row, 3].Value?.ToString().Trim();
-                    d.Tribunal = worksheet.Cells[row, 4].Value?.ToString().Trim();
-                    d.FormaDeIngreso = worksheet.Cells[row, 5].Value?.ToString().Trim();
-                    d.Materia = worksheet.Cells[row, 6].Value?.ToString().Trim();
-                    d.FechaDeDerivacion = worksheet.Cells[row, 7].Value?.ToString().Trim();
-                    d.FechaDeAudienciaReal = worksheet.Cells[row, 8].Value?.ToString().Trim();
-                    d.Asignado = worksheet.Cells[row, 9].Value?.ToString().Trim();
-                    d.FechaDeAntecedentes = worksheet.Cells[row, 10].Value?.ToString().Trim();
-                    d.AntecedentesEnviados = worksheet.Cells[row, 11].Value?.ToString().Trim();
-                    d.Pjud = worksheet.Cells[row, 12].Value?.ToString().Trim();
-                    d.Folio = worksheet.Cells[row, 13].Value?.ToString().Trim();
-                    d.DireccionDeCorreo = worksheet.Cells[row, 14].Value?.ToString().Trim();
+                    for (int row = 1; row <= rowCount; row++)
+                    {
+                        Derivacion d = new Derivacion();
 
+                        d.RolOficio = worksheet.Cells[row, 1].Value?.ToString().Trim();
+                        d.Partes = worksheet.Cells[row, 2].Value?.ToString().Trim();
+                        d.Isapre = worksheet.Cells[row, 3].Value?.ToString().Trim();
+                        d.Tribunal = worksheet.Cells[row, 4].Value?.ToString().Trim();
+                        d.FormaDeIngreso = worksheet.Cells[row, 5].Value?.ToString().Trim();
+                        d.Materia = worksheet.Cells[row, 6].Value?.ToString().Trim();
+                        d.FechaDeDerivacion = worksheet.Cells[row, 7].Value?.ToString().Trim();
+                        d.FechaDeAudienciaReal = worksheet.Cells[row, 8].Value?.ToString().Trim();
+                        d.Asignado = worksheet.Cells[row, 9].Value?.ToString().Trim();
+                        d.FechaDeAntecedentes = worksheet.Cells[row, 10].Value?.ToString().Trim();
+                        d.AntecedentesEnviados = worksheet.Cells[row, 11].Value?.ToString().Trim();
+                        d.Pjud = worksheet.Cells[row, 12].Value?.ToString().Trim();
+                        d.Folio = worksheet.Cells[row, 13].Value?.ToString().Trim();
+                        d.DireccionDeCorreo = worksheet.Cells[row, 14].Value?.ToString().Trim();
 
+                        listadoDeDerivaciones.Add(d);
 
-                    listadoDeDerivaciones.Add(d);   
-
+                    }
                 }
 
-
-
-
-
             }
-            
-
-
+           
             return listadoDeDerivaciones;
 
         }
